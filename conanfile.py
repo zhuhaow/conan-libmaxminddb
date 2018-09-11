bde4aa23af979e962701857d0b6214d2defd29ec
zhuhaow <zhuhaow@gmail.com>
2018-09-11 22:02:24 +0800

    Support for *nix

---
+++ conanfile.py
@@ -0,0 +1,42 @@
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
+
+
+class LibmaxminddbConan(ConanFile):
+    name = "libmaxminddb"
+    version = "1.3.2"
+    license = "Apache License, Version 2.0"
+    url = "https://github.com/maxmind/libmaxminddb"
+    description = "C library for the MaxMind DB file format"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = ("shared=False", "fPIC=True")
+
+    def source(self):
+        self.run("git clone --recursive https://github.com/maxmind/libmaxminddb")
+        self.run("cd libmaxminddb && git checkout 1.3.2")
+
+    def build(self):
+        with tools.chdir("libmaxminddb"):
+            if self.settings.os != "Windows":
+                self.run("autoreconf -fiv", run_environment=True)
+
+                args = ['--prefix=%s' % self.package_folder]
+                if self.options.shared:
+                    args.extend(['--disable-static', '--enable-shared'])
+                else:
+                    args.extend(['--disable-shared', '--enable-static'])
+                if self.settings.build_type == 'Debug':
+                    args.append('--enable-debug')
+                if self.options.fPIC:
+                    args.append('--with-pic')
+
+                autotools = AutoToolsBuildEnvironment(self)
+                autotools.configure(args=args)
+                autotools.make()
+                autotools.install()
+            else:
+                raise Exception("Windows is not supported.")
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+
