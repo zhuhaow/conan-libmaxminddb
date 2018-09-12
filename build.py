1e8749e5c81d41c83d88b2593ca558793650332f
zhuhaow <zhuhaow@gmail.com>
2018-09-12 22:51:59 +0800

    Fix set up arch for Appveyor

--- build.py
+++ build.py
@@ -1,6 +1,11 @@
 from conan.packager import ConanMultiPackager
+import platform
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    if platform.system() == "Windows":
+        builder = ConanMultiPackager(default_archs=["Win32", "x64_64"])
+    else:
+        builder = ConanMultiPackager()
+
     builder.add_common_builds(shared_option_name="libmaxminddb:shared")
     builder.run()
