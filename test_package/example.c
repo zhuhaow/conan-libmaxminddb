89ee07d38aadab7a86532e08d79698eaf91fa696
zhuhaow <zhuhaow@gmail.com>
2018-09-12 23:44:05 +0800

    Set up test

---
+++ test_package/example.c
@@ -0,0 +1,19 @@
+#include <libmaxminddb.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  MMDB_s *mmdb = (MMDB_s *)calloc(1, sizeof(MMDB_s));
+
+  if (NULL == mmdb) {
+    exit(1);
+  }
+
+  int status = MMDB_open("file_does_not_exist", MMDB_MODE_MMAP, mmdb);
+
+  if (status != MMDB_FILE_OPEN_ERROR) {
+    exit(1);
+  }
+
+  return 0;
+}
